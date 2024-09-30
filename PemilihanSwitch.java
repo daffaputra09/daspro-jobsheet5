a5b27c78eab2c26f0f5cfb3bd625681408717f9e
daffaputra09 <daffaputra863@gmail.com>
2024-09-30 18:11:55 +0700

    menambahkan handler operator tidak sesuai

--- PemilihanSwitch.java
+++ PemilihanSwitch.java
@@ -5,14 +5,17 @@ public class PemilihanSwitch {
         Scanner input = new Scanner(System.in);
         double angka1, angka2, hasil = 0;
         char operator;
+        
         System.out.print("Masukkan angka 1: ");
         angka1 = input.nextDouble();
+        
         System.out.print("Masukkan angka 2: ");
         angka2 = input.nextDouble();
+        
         System.out.print("Masukkan operator (+ - * /): ");
         operator = input.next().charAt(0);
 
-         switch (operator) {
+        switch (operator) {
             case '+':
                 hasil = angka1 + angka2;
                 break;
@@ -25,7 +28,11 @@ public class PemilihanSwitch {
             case '/':
                 hasil = angka1 / angka2;
                 break;
-         }
-         System.out.println(angka1 + " " + operator + " " + angka2 + " = " + hasil);
+            default:
+                System.out.println("Operator yang dipilih tidak sesuai.");
+                return; 
+        }
+
+        System.out.println(angka1 + " " + operator + " " + angka2 + " = " + hasil);
     }
 }
